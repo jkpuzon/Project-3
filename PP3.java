488393aa3092b2ef7c7a1838e72371275e21a74e
zkowal31 <61563543+zkowal31@users.noreply.github.com>
2020-04-08 17:02:57 -0400

    Add files via upload

---
+++ PP3.java
@@ -0,0 +1,16 @@
+
+public class PP3 {
+
+	@Override
+	public String toString() {
+		
+		String str = "";
+		Node n = head;
+		
+		while(n != null) {
+			str = str + n.get(element);
+			n = n.getNext();
+		}
+		return str;
+	}
+}
